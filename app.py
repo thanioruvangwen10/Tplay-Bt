30c3e86450749d3241c749ba04f14838c2c6e1f3
thanioruvangwen10 <massayalaan@gmail.com>
2024-07-18 00:18:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 8000)
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
